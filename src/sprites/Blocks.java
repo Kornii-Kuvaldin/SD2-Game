0fb261beffaa95cb219ec0c1b36773a7fabc934d
Kornii Kuvaldin <kornii.kuvaldin@student.griffith.ie>
2025-04-02 10:01:09 +0100

    Added blocks class

---
+++ src/sprites/Blocks.java
@@ -0,0 +1,13 @@
+package sprites;
+
+public class Blocks extends Sprite {
+
+	private double hardness;
+	private double value;
+	
+	public Blocks(String fileName, int x, int y, int width, int height) {
+		super(fileName, x, y, width, height);
+	}
+	
+
+}
