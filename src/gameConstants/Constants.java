d567addae865e0539847f21eed56450737abe559
Eryel-32 <leyresaplaya@gmail.com>
2025-04-04 11:29:40 +0100

    Changed the height of the ground

--- src/gameConstants/Constants.java
+++ src/gameConstants/Constants.java
@@ -14,7 +14,7 @@ public class Constants {
 	
 	//size
 	public final static Dimension SCREEN_SIZE =  Toolkit.getDefaultToolkit().getScreenSize();
-	public final static int GROUND_HEIGHT  = SCREEN_SIZE.height/3;
+	public final static int GROUND_HEIGHT  = SCREEN_SIZE.height/10;
 	public final static int SCREEN_WIDTH = SCREEN_SIZE.width;
 	public final static int SCREEN_HEIGHT = SCREEN_SIZE.height;
 
