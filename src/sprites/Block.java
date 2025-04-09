384c773263d9fc77554ac00fd29cee78e527383b
Kornii-Kuvaldin <dumbicefaiy@gmail.com>
2025-04-09 09:45:53 +0100

    Add previously missing methods from block class

--- src/sprites/Block.java
+++ src/sprites/Block.java
@@ -1,13 +1,28 @@
 package sprites;
 
-public class Blocks extends Sprite {
+import sprites.Sprite;
+
+public class Block extends Sprite {
 
 	private double hardness;
 	private double value;
-	
-	public Blocks(String fileName, int x, int y, int width, int height) {
+	private double progress=0;
+	private boolean isBroken=false;
+
+	public Block(String fileName, int x, int y, int width, int height) {
 		super(fileName, x, y, width, height);
 	}
-	
+	public void blockMine() {
+		progress += 10;
+		if (progress>=hardness)
+			isBroken=true;
+	}
+	public boolean getBroken() {
+		return isBroken;
+	}
+	public void setHardness(double x) {
+		hardness=x;
+	}
+
 
 }
