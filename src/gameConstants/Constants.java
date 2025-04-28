b171f4712a22ad9e8edddfd78e2f230104802a6b
Kornii Kuvaldin <kornii.kuvaldin@student.griffith.ie>
2025-04-28 15:48:29 +0100

    Changed blocks to a square

--- src/gameConstants/Constants.java
+++ src/gameConstants/Constants.java
@@ -29,8 +29,8 @@ public class Constants {
 	//Player
 
 
-	public final static int PLAYER_WIDTH = 50;
-	public final static int PLAYER_HEIGHT = 50;
+	public final static int PLAYER_WIDTH = 100;
+	public final static int PLAYER_HEIGHT = 100;
 	public final static int PLAYER_START_X = 0;
 	public final static int PLAYER2_START_X = 200;
 	public final static int PLAYER_SPEED = 2;
@@ -38,7 +38,7 @@ public class Constants {
 	public final static int PLAYER_FALL_SPEED = 4;
 	
 	//Enemy
-	public final static int ENEMY_SIZE = 50;
+	public final static int ENEMY_SIZE = 100;
 	public final static int ENEMY_START_X = 200;
 	public final static int ENEMY_SPEED = 3;
 	
@@ -47,8 +47,8 @@ public class Constants {
 	public final static int COIN_SCORE = 1;
 	
 	//Block
-	public final static int BLOCK_WIDTH = 100;
-	public final static int BLOCK_HEIGHT = 50;
+	public final static int BLOCK_WIDTH = 64;
+	public final static int BLOCK_HEIGHT = 64;
 	
 	//speed
 
