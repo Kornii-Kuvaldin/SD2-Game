69818ddd880c26f65ba1520db51cbccc82706b8d
Kornii Kuvaldin <kornii.kuvaldin@student.griffith.ie>
2025-04-29 09:30:55 +0100

    Changed player size to match block size so they can fit between two

--- src/gameConstants/Constants.java
+++ src/gameConstants/Constants.java
@@ -29,8 +29,8 @@ public class Constants {
 	//Player
 
 
-	public final static int PLAYER_WIDTH = 100;
-	public final static int PLAYER_HEIGHT = 100;
+	public final static int PLAYER_WIDTH = 64;
+	public final static int PLAYER_HEIGHT = 64;
 	public final static int PLAYER_START_X = 0;
 	public final static int PLAYER2_START_X = 200;
 	public final static int PLAYER_SPEED = 2;
